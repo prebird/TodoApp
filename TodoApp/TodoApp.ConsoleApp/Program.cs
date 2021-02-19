7a3f5d60fc1ca1d61b873269b8df8d6060be9f12
yonggyujeong <yonggyujeong10@gmail.com>
2021-02-19 16:26:48 +0900

    56. 인메모리 데이터베이스 프로그래밍 맛보기

---
+++ TodoApp/TodoApp.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Models;
+
+namespace TodoApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITodoRepository _repository = new TodoRepositoryInMemory();
+            List<Todo> todos = new List<Todo>();
+            todos = _repository.GetAll(); //가져오기
+            //[1] 기본데이터 출력
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id}-{t.Title}({t.IsDone})");
+            }
+            //[2] 데이터 입력
+            Todo todo = new Todo { Title = "Database", IsDone = true };
+            _repository.Add(todo);
+            todos = _repository.GetAll(); //다시 로드
+
+            //[3] 변경 데이터 출력
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id}-{t.Title}({t.IsDone})");
+            }
+        }
+    }
+}
---
+++ TodoApp/TodoApp.Models/TodoRepositoryInMemory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TodoApp.Models
+{
+    public class TodoRepositoryInMemory : ITodoRepository
+    {
+        private static List<Todo> _todos = new List<Todo>();
+
+        public TodoRepositoryInMemory()
+        {
+            _todos = new List<Todo>
+            {
+                new Todo {Id = 1, Title = "ASP.NET Core학습", IsDone = false},
+                new Todo {Id = 2, Title = "Blazor 학습", IsDone = false},
+                new Todo {Id = 3, Title = "C# 학습", IsDone = true}
+            };
+        }
+
+        //인-메모리 데이터베이스 사용 영역
+        public void Add(Todo model)
+        {
+            //현재 DB, 컬랙션에 있는 ID중 최댓값 +1 구하기
+            model.Id = _todos.Max(t => t.Id) + 1;
+            _todos.Add(model); //메서드 선언중인 메서드를 사용??
+        }
+
+        public List<Todo> GetAll()
+        {
+            return _todos.ToList(); //tolist 안해도 됨 _todo 가 리스트라
+        }
+    }
+} //_todo가 하나의 인메모리 데이터베이스임(하나의 컬렉션만 담을수 잇는)
